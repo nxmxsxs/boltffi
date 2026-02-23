3317541b72293c0fd7bc22dfa00538a24243f7cc
Ali A. Hilal <ali94_200970@yahoo.com>
2026-02-23 18:20:12 +0100

    use macro to conform scalars to passable

--- boltffi_core/src/passable.rs
+++ boltffi_core/src/passable.rs
@@ -9,96 +9,20 @@ pub unsafe trait Passable: Sized {
     fn pack(self) -> Self::Out;
 }
 
-unsafe impl Passable for i8 {
-    type In = i8;
-    type Out = i8;
-    unsafe fn unpack(input: i8) -> Self { input }
-    fn pack(self) -> i8 { self }
+macro_rules! impl_passable_scalar {
+    ($($ty:ty),*) => {
+        $(
+            unsafe impl Passable for $ty {
+                type In = $ty;
+                type Out = $ty;
+                unsafe fn unpack(input: $ty) -> Self { input }
+                fn pack(self) -> $ty { self }
+            }
+        )*
+    };
 }
 
-unsafe impl Passable for i16 {
-    type In = i16;
-    type Out = i16;
-    unsafe fn unpack(input: i16) -> Self { input }
-    fn pack(self) -> i16 { self }
-}
-
-unsafe impl Passable for i32 {
-    type In = i32;
-    type Out = i32;
-    unsafe fn unpack(input: i32) -> Self { input }
-    fn pack(self) -> i32 { self }
-}
-
-unsafe impl Passable for i64 {
-    type In = i64;
-    type Out = i64;
-    unsafe fn unpack(input: i64) -> Self { input }
-    fn pack(self) -> i64 { self }
-}
-
-unsafe impl Passable for u8 {
-    type In = u8;
-    type Out = u8;
-    unsafe fn unpack(input: u8) -> Self { input }
-    fn pack(self) -> u8 { self }
-}
-
-unsafe impl Passable for u16 {
-    type In = u16;
-    type Out = u16;
-    unsafe fn unpack(input: u16) -> Self { input }
-    fn pack(self) -> u16 { self }
-}
-
-unsafe impl Passable for u32 {
-    type In = u32;
-    type Out = u32;
-    unsafe fn unpack(input: u32) -> Self { input }
-    fn pack(self) -> u32 { self }
-}
-
-unsafe impl Passable for u64 {
-    type In = u64;
-    type Out = u64;
-    unsafe fn unpack(input: u64) -> Self { input }
-    fn pack(self) -> u64 { self }
-}
-
-unsafe impl Passable for f32 {
-    type In = f32;
-    type Out = f32;
-    unsafe fn unpack(input: f32) -> Self { input }
-    fn pack(self) -> f32 { self }
-}
-
-unsafe impl Passable for f64 {
-    type In = f64;
-    type Out = f64;
-    unsafe fn unpack(input: f64) -> Self { input }
-    fn pack(self) -> f64 { self }
-}
-
-unsafe impl Passable for bool {
-    type In = bool;
-    type Out = bool;
-    unsafe fn unpack(input: bool) -> Self { input }
-    fn pack(self) -> bool { self }
-}
-
-unsafe impl Passable for usize {
-    type In = usize;
-    type Out = usize;
-    unsafe fn unpack(input: usize) -> Self { input }
-    fn pack(self) -> usize { self }
-}
-
-unsafe impl Passable for isize {
-    type In = isize;
-    type Out = isize;
-    unsafe fn unpack(input: isize) -> Self { input }
-    fn pack(self) -> isize { self }
-}
+impl_passable_scalar!(i8, i16, i32, i64, u8, u16, u32, u64, f32, f64, bool, usize, isize);
 
 unsafe impl Passable for String {
     type In = FfiSpan;
